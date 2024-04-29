daf84e66f54b39c8fdd7cf583f9ae1fcf8bfaacc
mcekov <mtsekov@zohomail.eu>
2024-04-29 08:26:25 +0300

    Add lang update

--- src/components/Lang.jsx
+++ src/components/Lang.jsx
@@ -1,13 +1,36 @@
+"use client";
+
+import React, { useState } from "react";
+import { cn } from "@/lib/utils";
 import { ChevronUp } from "lucide-react";
-import React from "react";
 
 const Lang = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="flex gap-2 items-center">
-      <span className="fi fi-gb"></span>
-      <span className="text-white text-[14px]">EN</span>
-      <ChevronUp size={15} className="text-gray-600" />
-    </div>
+    <>
+      <div className="flex gap-2 items-center relative">
+        <span className="fi fi-gb" />
+        <span className="text-white text-[14px]">EN</span>
+        <ChevronUp
+          size={15}
+          className="text-gray-600"
+          onClick={() => setIsOpen(!isOpen)}
+        />
+
+        {/*  <ul
+          className={cn(
+            `bg-[#e0e0e0] px-10 py-10 max-h-screen shadow-md absolute right-0 top-10 transition-opacity duration-100 z-20`,
+            {
+              "opacity-0": !isOpen,
+              "opacity-100": isOpen,
+            }
+          )}
+        >
+          <li>lang</li>
+        </ul> */}
+      </div>
+    </>
   );
 };
 
